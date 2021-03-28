fd3078e82b231f1bc3994c3f503b92227b337abb
Junaed Rayhan <junaedrayhan@gmail.com>
2021-03-29 03:42:09 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Inventory = () => {
+     const handleAddProduct = () =>{
+         const product = {}
+         fetch('https://secret-mountain-07404.herokuapp.com/addProduct',{
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(product)
+         })
+     }
+    return (
+        <div>
+            <form action="">
+                <p><span>Name</span><input type="text"/></p>
+                <p><span>Price:</span><input type="text"/></p>
+                <p><span>Quantity:</span><input type="text"/></p>
+                <p><span>Product Image:</span><input type="file"/></p>
+            </form>
+            <button onClick={handleAddProduct}>Add Product</button>
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClbNbA6GnTEm5NlKY5GV8fiGRdlnacPG0",
+    authDomain: "react-simple-b0fc7.firebaseapp.com",
+    projectId: "react-simple-b0fc7",
+    storageBucket: "react-simple-b0fc7.appspot.com",
+    messagingSenderId: "968722582557",
+    appId: "1:968722582557:web:daa9470732246d32540af4"
+  };
+  export default firebaseConfig;
